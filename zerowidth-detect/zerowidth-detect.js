b1037828e68149baf3b4fcadf55b7a2d67a882b1
magn4951 <32102827+magn4951@users.noreply.github.com>
2018-04-06 10:38:21 +0200

    Fixing regex url match

--- zerowidth-detect/zerowidth-detect.js
+++ zerowidth-detect/zerowidth-detect.js
@@ -17,16 +17,17 @@ browser.storage.local.get("whitelist").then(function(result) {
     }
 
     var split = result.whitelist.split("|");
+    console.log(split);
     for (var index in split) {
         if (split[index]) {
             console.log(split[index]);
             var domain = split[index];
             domain = domain.replace(/\*/i, "([\w\.]+)");
             console.log("|"+domain+"|");
-            var pattern = "^https?://(www.)?"+ domain +"/"; //new RegExp("^(http|https)://(www.)?"+ domain +"/", "i");
+            var pattern = new RegExp("^https?:\/\/(www\.)?"+domain+"(\/.*)?", "i")
             var url = window.location.href;
             console.log("cri");
-            /*if (url.test(pattern) {
+            if (pattern.test(url)) {
                 console.log("TRUE");
                 browser.runtime.sendMessage({
                     type: "html",
@@ -35,7 +36,7 @@ browser.storage.local.get("whitelist").then(function(result) {
                 }).then(handleResponse, handleError);
             } else {
                 console.log("FALSE");
-            }*/
+            }
         }
     }
 }, handleError);
